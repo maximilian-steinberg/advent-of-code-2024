f9475eeb402e94523db51b0feb8ce28d81789262
Maximilian Steinberg <msteinberg@progeek.de>
2024-12-03 12:49:23 +0100

    Complete Day01, Day02

--- src/day_03/Day03.kt
+++ src/day_03/Day03.kt
@@ -7,21 +7,42 @@ fun main() {
 
 
     fun part1(input: List<String>): Int {
-        var countSafeInputs = 0
+        val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+        var sum = 0
 
-        input.map {
-            val levels = it.split(" ")
+        input.forEach { s ->
+            regex.findAll(s).forEach { r ->
+                sum += r.groupValues[1].toInt() * r.groupValues[2].toInt()
+            }
         }
 
-        return 0
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val regex = """mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""".toRegex()
+        var sum = 0
+        var isEnabled = true
+
+        input.forEach { s ->
+            regex.findAll(s).forEach { r ->
+                if(r.value == "do()") {
+                    isEnabled = true
+                } else if (r.value == "don't()") {
+                    isEnabled = false
+                } else {
+                    if (isEnabled) {
+                        sum += r.groupValues[1].toInt() * r.groupValues[2].toInt()
+                    }
+                }
+            }
+        }
+
+        return sum
     }
 
     val testInput = readInput("day_03/test")
-    check(part1(testInput) == 2)
+    //check(part1(testInput) == 2)
 
     // Read the input from the `src/input.txt` file.
     val input = readInput("day_03/input")
